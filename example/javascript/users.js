141f009a037acc13614b4b704190eb58c574887f
Evan Shortiss <evanshortiss@gmail.com>
2019-06-27 14:53:42 -0700

    chore: update example structure

---
+++ example/javascript/users.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = [
+  { id: 1000, name: 'anne, a.', age: 25 },
+  { id: 1001, name: 'barry, a.', age: 52 },
+  { id: 1002, name: 'clare, a.', age: 25 },
+  { id: 1003, name: 'joe, a.', age: 67 },
+  { id: 1004, name: 'anne, b.', age: 47 },
+  { id: 1005, name: 'barry, b.', age: 80 },
+  { id: 1006, name: 'clare, b.', age: 28 },
+  { id: 1007, name: 'joe, b.', age: 15 }
+]
