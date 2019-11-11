48d914df4a5aca41598db4f6245245234fd98c8b
Evan Shortiss <evanshortiss@gmail.com>
2019-11-11 14:53:52 -0800

    fix: should still extend

--- express-joi-validation.d.ts
+++ express-joi-validation.d.ts
@@ -24,7 +24,7 @@ export enum ContainerTypes {
  * Use this in you express error handler if you've set *passError* to true
  * when calling *createValidator*
  */
-export interface ExpressJoiError {
+export interface ExpressJoiError extends Joi.ValidationResult {
   type: ContainerTypes
 }
 
